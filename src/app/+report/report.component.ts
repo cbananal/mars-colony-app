ecf8c47e49112172b151cdd7491616876d2a3c5c
Claudine <claudine.bananal@gmail.com>
2016-06-04 15:56:12 -0700

    pull in alien types on report page

--- src/app/+report/report.component.ts
+++ src/app/+report/report.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { IAlien } from '../shared/models';
+import { AlienService } from '../shared/services';
 @Component({
   moduleId: module.id,
   selector: 'app-report',
   templateUrl: 'report.component.html',
   styleUrls: ['report.component.css'],
-  directives: [ ROUTER_DIRECTIVES ]
+  directives: [ ROUTER_DIRECTIVES ],
+  providers: [AlienService]
 })
 export class ReportComponent implements OnInit {
 
-  constructor() {}
+  public aliens: IAlien[];
+
+  constructor(
+    private alienService: AlienService
+  ) {}
 
   ngOnInit() {
+    this.alienService.getAliens().then( alien => this.aliens = alien);
   }
 
 }
